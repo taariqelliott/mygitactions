b68e8b9740672d90f9d455b8da50b5e93bfafd27
Taariq Elliott <codebytaariq@gmail.com>
2024-08-14 13:21:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ This module contains two simple functions: hello and bye.
 Each function prints a greeting or farewell message.
 """
 
+
 def hello():
     """Prints a greeting message."""
     print("hi")
